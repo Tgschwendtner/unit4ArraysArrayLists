36478422f83d4fd5b9bee54678c794aa628b2a99
Tgschwendtner <tgschwendtner@NN121-HA-05.HS.sd203.org>
2015-11-30 10:15:48 -0600

    11/30/15

--- Pay/Hourly_Pay.java
+++ Pay/Hourly_Pay.java
@@ -13,37 +13,50 @@ public class Hourly_Pay
         System.out.print ("What is your hourly Pay? ");
         Hourlypay = scan.nextDouble();
         //Gets Days worked
-        System.out.print ("Days did you work");
+        System.out.print ("Days did you work? ");
         days = scan.nextInt();
         //Makes array Length = days worked
         double[] Hoursworked = new double[days];
         double[] MinutesWorked = new double[days];
+        System.out.print ("\n");
         for (int i = 0; i < days; i++)
         {
             if (i == 0)
             {
                 System.out.print ("How many hours did you work on the first day? ");
-                hours = scan.nextDouble();
+                hours = scan.nextInt();
                 System.out.print ("How many minutes did you work on the first day? ");
-                minutes = scan.nextDouble();
+                minutes = scan.nextInt();
+                System.out.print ("\n");
             }
             else
             {
                 System.out.print ("How many hours did you work on the next day? ");
-                hours = scan.nextDouble();
+                hours = scan.nextInt();
                 System.out.print ("How many minutes did you work on the next day? ");
-                minutes = scan.nextDouble();
+                minutes = scan.nextInt();
+                System.out.print ("\n");
             }
             Hoursworked[i] = hours;
             MinutesWorked[i] = minutes;
         }
         //Adds up all the hours
-        
+        double totalhours = 0;
+        double totalminutes = 0;
+        for (int i = 0; i <= days - 1; i++)
+        {
+            totalhours = totalhours + Hoursworked[i];
+        }
+        for (int m = 0; m <= days - 1; m++)
+        {
+            totalminutes = totalminutes + MinutesWorked[m];
+        }
         //Calculates total pay depending on users hourly pay input
-        
+        totalminutes = totalminutes+ (totalhours*60);
+        double totaltime = (totalminutes/60);
+        double totalpay = (totaltime*Hourlypay);
         //print the ammount
-        System.out.println( Arrays.toString( Hoursworked ));
-        System.out.println( Arrays.toString( MinutesWorked ));
+        System.out.println("$"+ totalpay);
     }
 }
 
--- Pay/PArtialArray.java
+++ Pay/PArtialArray.java
@@ -1,4 +1,4 @@
-public class PArtialArray
+public class PartialArray
 {
     private int[] values;
     private int currentSize;
@@ -7,13 +7,61 @@ public class PArtialArray
     {
         //initialise instance variables
         this.values = new int[100];
-
         for ( this.currentSize = 0;
         this.currentSize < 10;
         this.currentSize++)
         {
             this.values[this.currentSize] =
-            this..currentSize * this..currentSize;
+            this.currentSize * this.currentSize;
         }
+    }
+    public void remove( int pos)
+    {
+        for (int i = pos + 1; i < currentSize; i++)
+        {
+            values[ i - 1 ] = values[ i ];
+        }
+        currentSize--;
+    }
+    public void insert ( int pos, int newValue)
+    {
+        if( this.currentSize == this.values.length )
+        {
+            this.grow();
+        }
+        if(this.currentSize < this.values.length)
+        {
+            this.currentSize++;
+            for ( int i = this.currentSize - 1;
+            i > pos;
+            i--)
+            {
+                this.values[i] = this.values[ i -1];
+            }
+            this.values[ pos ] = newValue;
+        }
+    }
+    public void swap( int pos1, int pos2)
+    {
+        if (pos1 < this.currentSize && pos2 < this.currentSize)
+        {
+            int temp = this.values[pos1 ];
+            this.values[ pos1 ] = this.values[ pos2 ];
+            this.values[ pos2] = temp;
+        }
+    }
+    private void grow()
+    {
+        int[] newValues = new int[ this.currentSize * 2];
+        for( int i = 0; i < this.currentSize; i++)
+        {
+            newValues[ i ] = this.values[ i ];
+        }
+        this.values = newValues;
+    }
+    public static void main( String[] args)
+    {
+        PartialArray myPartialArray = new PartialArray();
+        
     }
 }
